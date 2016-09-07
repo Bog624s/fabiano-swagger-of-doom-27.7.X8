3c607eac0703c035c8dffd4f9d7137b67cb3126a
GhostMaree <markus098@o2.pl>
2016-09-07 19:51:10 +0200

    Removed Client Updater

--- server_src/Client Updater/rabcdasm/01.bat
+++
@@ -1,3 +0,0 @@
-swfdecompress client.swf
-abcexport client.swf
-rabcdasm client-0.abc
--- server_src/Client Updater/rabcdasm/02.bat
+++
@@ -1,3 +0,0 @@
-rabcasm client-0\client-0.main.asasm
-abcreplace client.swf 0 client-0\client-0.main.abc
-pause
