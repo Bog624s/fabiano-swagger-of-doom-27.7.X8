7e83686850e972bb884c83d8006934235f475b4e
GhostMaree <markus098@o2.pl>
2016-11-30 19:40:19 +0100

    huh

--- wServer/networking/svrPackets/AoEPacket.cs
+++ wServer/networking/svrPackets/AoEPacket.cs
@@ -1,6 +1,6 @@
 ﻿namespace wServer.networking.svrPackets
 {
-    public class AOEPacket : ServerPacket
+    public class AoePacket : ServerPacket
     {
         public Position Position { get; set; }
         public float Radius { get; set; }
@@ -8,16 +8,12 @@
         public ConditionEffectIndex Effects { get; set; }
         public float EffectDuration { get; set; }
         public short OriginType { get; set; }
-        //public ARGB Color { get; set; }
 
-        public override PacketID ID
-        {
-            get { return PacketID.AOE; }
-        }
+        public override PacketID ID => PacketID.AOE;
 
         public override Packet CreateInstance()
         {
-            return new AOEPacket();
+            return new AoePacket();
         }
 
         protected override void Read(Client psr, NReader rdr)
@@ -25,10 +21,9 @@
             Position = Position.Read(psr, rdr);
             Radius = rdr.ReadSingle();
             Damage = rdr.ReadUInt16();
-            Effects = (ConditionEffectIndex) rdr.ReadByte();
+            Effects = (ConditionEffectIndex)rdr.ReadByte();
             EffectDuration = rdr.ReadSingle();
             OriginType = rdr.ReadInt16();
-            //Color = ARGB.Read(psr, rdr);
         }
 
         protected override void Write(Client psr, NWriter wtr)
@@ -36,10 +31,9 @@
             Position.Write(psr, wtr);
             wtr.Write(Radius);
             wtr.Write(Damage);
-            wtr.Write((byte) Effects);
+            wtr.Write((byte)Effects);
             wtr.Write(EffectDuration);
             wtr.Write(OriginType);
-            //Color.Write(psr, wtr);
         }
     }
 }
